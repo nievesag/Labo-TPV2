776e1669cd6a54793414abf990edd4f2d86e4b9b
nievesag <nievalon@ucm.es>
2024-04-06 01:31:34 +0200

    is inmune

--- PRACTICA 2/pacman/TPV2/TPV2/src/components/IsMiraculousComponent.h
+++ PRACTICA 2/pacman/TPV2/TPV2/src/components/IsMiraculousComponent.h
@@ -1,5 +1,23 @@
 #pragma once
-class IsMiraculousComponent
-{
-};
 
+#include "../ecs/Component.h"
+
+class IsMiraculousComponent : public ecs::Component
+{
+	__CMPID_DECL__(ecs::cmp::INMUNE)
+
+	IsMiraculousComponent() : isInmune() {}
+	virtual ~IsMiraculousComponent() {}
+
+	bool isInmune;
+
+	bool getIsInmune()
+	{
+		return isInmune;
+	}
+
+	void setInmune(bool i)
+	{
+		isInmune = i;
+	}
+};
--- PRACTICA 2/pacman/TPV2/TPV2/src/systems/FoodSystem.cpp
+++ PRACTICA 2/pacman/TPV2/TPV2/src/systems/FoodSystem.cpp
@@ -25,66 +25,66 @@ void FoodSystem::setFruits()
 	//
 	auto& rand = sdlutils().rand();
 
-	if (ghostCD + lastSpawn < sdlutils().virtualTimer().currTime()
-		&& currentGhosts_ < ghostLimit_) {
+	//if (ghostCD + lastSpawn < sdlutils().virtualTimer().currTime()
+	//	&& currentGhosts_ < ghostLimit_) {
 
-		lastSpawn = sdlutils().virtualTimer().currTime();
+	//	lastSpawn = sdlutils().virtualTimer().currTime();
 
-		// add and entity to the manager
-				//
-		auto e = mngr_->addEntity(ecs::grp::GHOSTS);
+	//	// add and entity to the manager
+	//			//
+	//	auto e = mngr_->addEntity(ecs::grp::GHOSTS);
 
-		// add a Transform component, and initialise it with random size and position
-		//
-		auto tr = mngr_->addComponent<Transform>(e);
+	//	// add a Transform component, and initialise it with random size and position
+	//	//
+	//	auto tr = mngr_->addComponent<Transform>(e);
 
-		// add an Image Component
-		//
-		auto img = mngr_->addComponent<Image>(e, &sdlutils().images().at("tennis_ball"));
+	//	// add an Image Component
+	//	//
+	//	auto img = mngr_->addComponent<Image>(e, &sdlutils().images().at("tennis_ball"));
 
-		// image with frames setup
-		//
-		int width = 40;
-		int height = 40;
+	//	// image with frames setup
+	//	//
+	//	int width = 40;
+	//	int height = 40;
 
-		tr->width_ = width;
-		tr->height_ = height;
+	//	tr->width_ = width;
+	//	tr->height_ = height;
 
-		int pos = sdlutils().rand().nextInt(0, 4);
+	//	int pos = sdlutils().rand().nextInt(0, 4);
 
-		switch (pos)
-		{
-		case 0:
-			// esquina superior izq 
+	//	switch (pos)
+	//	{
+	//	case 0:
+	//		// esquina superior izq 
 
-			tr->pos_.setX(0);
-			tr->pos_.setY(0);
+	//		tr->pos_.setX(0);
+	//		tr->pos_.setY(0);
 
-			break;
-		case 1:
-			// esquina superior derecha
+	//		break;
+	//	case 1:
+	//		// esquina superior derecha
 
-			tr->pos_.setX(0);
-			tr->pos_.setY(sdlutils().height() - height);	// - height
-			break;
-		case 2:
-			// esquina inferior izquierda
+	//		tr->pos_.setX(0);
+	//		tr->pos_.setY(sdlutils().height() - height);	// - height
+	//		break;
+	//	case 2:
+	//		// esquina inferior izquierda
 
-			tr->pos_.setX(sdlutils().width() - width);	// - width
-			tr->pos_.setY(0);
-			break;
-		case 3:
-			// esquina inferior derecha
+	//		tr->pos_.setX(sdlutils().width() - width);	// - width
+	//		tr->pos_.setY(0);
+	//		break;
+	//	case 3:
+	//		// esquina inferior derecha
 
-			tr->pos_.setX(sdlutils().width() - width);	// - width
-			tr->pos_.setY(sdlutils().height() - height);	// - height
-			break;
-		default:
-			break;
-		}
+	//		tr->pos_.setX(sdlutils().width() - width);	// - width
+	//		tr->pos_.setY(sdlutils().height() - height);	// - height
+	//		break;
+	//	default:
+	//		break;
+	//	}
 
-		currentGhosts_++;
-	}
+	//	currentGhosts_++;
+	//}
 }
 
 void FoodSystem::updateState()
