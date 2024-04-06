f2e27e95a153ddade204c1398e58f7a4a42d9756
nievesag <nievalon@ucm.es>
2024-04-06 02:22:13 +0200

    ismiraculous

--- PRACTICA 2/pacman/TPV2/TPV2/src/components/IsMiraculousComponent.cpp
+++ PRACTICA 2/pacman/TPV2/TPV2/src/components/IsMiraculousComponent.cpp
@@ -1,1 +1,26 @@
 #include "IsMiraculousComponent.h"
+#include "../sdlutils/SDLUtils.h"
+
+IsMiraculousComponent::IsMiraculousComponent()
+{
+	// inicializa la N frecuency
+	NFrecuency = sdlutils().rand().nextInt(minNFrecuency, maxNFrecuency);
+
+	// lo resetea tiempo y tipo de fruta
+	timeOfActivation = sdlutils().virtualTimer().currTime();
+	isMiraculous = false;
+}
+
+IsMiraculousComponent::~IsMiraculousComponent() {}
+
+void IsMiraculousComponent::MiraculousTransformation()
+{
+	// guarda el tiempo en el que lo has activado
+	timeOfActivation = sdlutils().virtualTimer().currTime();
+
+	// establece cuanto tiempo va a estar en ese estado
+	MDuration = sdlutils().rand().nextInt(minMDuration, maxMDuration);
+
+	// es miraculousa a true
+	isMiraculous = true;
+}
